1bd4b1697e20bdfb1fe6e725793734d564ccb573
chiaoli <hussar91522@gmail.com>
2025-07-13 15:44:58 +0800

    Feat(mappings): add custom key bindings
    - <leader>e: 切換檔案管理器 (NvimTreeToggle)
    - <leader>w: 儲存檔案
    - <leader>q: 退出
    
    提升操作效率，提供常用功能的快速存取

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,8 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+map("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+map("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
