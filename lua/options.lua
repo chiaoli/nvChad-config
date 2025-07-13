75cf9ebbe348107c903ef8c0a7329573be27e60d
chiaoli <hussar91522@gmail.com>
2025-07-13 17:24:26 +0800

    Feat(options): enable relative line numbers
    啟用相對行號顯示：
    - 當前行顯示絕對行號
    - 其他行顯示相對距離
    - 便於快速相對移動操作

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,9 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+
+-- Enable relative line numbers
+o.relativenumber = true
+
 -- o.cursorlineopt ='both' -- to enable cursorline!
