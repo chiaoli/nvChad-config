2caef792467c4609856880e916355e12407b6b27
chiaoli <hussar91522@gmail.com>
2025-07-13 17:01:19 +0800

    Feat(navigation): implement advanced hop.nvim plugin
    新增 hop.nvim 快速跳轉套件：
    - s: 跳轉到字符 (HopChar1)
    - S: 跳轉到單詞 (HopWord)
    
    進階行內導航功能：
    - f/F: 行內向前/向後跳轉字符
    - t/T: 跳轉到字符前/後一位
    - 支援方向控制和偏移設定
    - current_line_only 限制在當前行
    
    視覺模式支援：
    - f: 視覺模式下整行跳轉選擇
    
    提供類似 Vim 原生 f/t 的體驗，但具備可視化跳轉標籤。

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -61,6 +61,40 @@ return {
     end,
   },
 
+  {
+    "smoka7/hop.nvim",
+    version = "*",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      -- Advanced hop keymaps with directions and line-only mode
+      local hop = require("hop")
+      local directions = require("hop.hint").HintDirection
+      local opts = { remap = true, silent = true }
+      
+      -- Basic hop commands
+      vim.keymap.set("n", "s", ":HopChar1<cr>", opts)
+      vim.keymap.set("n", "S", ":HopWord<cr>", opts)
+      
+      -- Enhanced f/F/t/T with hop for current line only
+      vim.keymap.set("", "f", function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+      end, { remap = true })
+      vim.keymap.set("", "F", function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+      end, { remap = true })
+      vim.keymap.set("", "t", function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+      end, { remap = true })
+      vim.keymap.set("", "T", function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+      end, { remap = true })
+      
+      -- Visual mode line hopping
+      vim.keymap.set("x", "f", "<cmd>HopLine<cr>", opts)
+    end,
+  },
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
